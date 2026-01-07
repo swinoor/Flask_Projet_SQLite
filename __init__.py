55248666fad61680e43bf1e8c399ed22506478e0
swinoor <milan.cassagnaud@icloud.com>
2026-01-07 17:35:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,25 +5,29 @@ from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
 
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)#m                                                                                                               
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
     return session.get('authentifie')
 
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
+@app.route('/fiche_nom')
+def fiche_nom():
+    # Récupère le nom depuis l'URL (?nom=DUPONT)
+    nom_saisi = request.args.get('nom', '')
+    
+    # Connexion à la base
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    
+    # Recherche (on utilise UPPER pour éviter les problèmes de majuscules)
+    client = conn.execute("SELECT * FROM clients WHERE nom = ?", (nom_saisi,)).fetchone()
+    conn.close()
+    
+    # On renvoie vers votre fichier
+    return render_template('recherche_client.html', client=client, nom_recherche=nom_saisi)
 
-@app.route('/lecture')
-def lecture():
-    if not est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('authentification'))
-
-  # Si l'utilisateur est authentifié
-    return "<h2>Bravo, vous êtes authentifié</h2>"
 
 @app.route('/authentification', methods=['GET', 'POST'])
 def authentification():
