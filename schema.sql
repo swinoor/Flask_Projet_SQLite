9839e99a4ae0c60cf07d7bbb86a226f9809f5f30
swinoor <milan.cassagnaud@icloud.com>
2026-02-18 15:07:42 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,16 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    stock INTEGER DEFAULT 1 -- Pour la gestion des stocks demandée
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT DEFAULT 'user' -- Pour le contrôle Admin/Utilisateur
 );
