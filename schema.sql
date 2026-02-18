baf22968f2aac93d7bf2d0e82086fe7a56b7d73e
swinoor <milan.cassagnaud@icloud.com>
2026-02-18 15:14:22 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS clients;
 CREATE TABLE clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,      -- Ce sera le TITRE du livre
-    prenom TEXT NOT NULL,   -- Ce sera l'AUTEUR du livre
-    adresse TEXT NOT NULL   -- Ce sera le STOCK (ex: "5 exemplaires")
+    nom TEXT NOT NULL,     
+    prenom TEXT NOT NULL,   
+    adresse TEXT NOT NULL   
 );
